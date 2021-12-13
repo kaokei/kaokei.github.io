bc2cfcfc2f6d00dce7c8afcf39e797125b1bbb62
kaokei <kaokei.rst@gmail.com>
2021-12-13 20:31:25 +0800

    修改用户名

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "Evan's blog",
+  title: "kaokei's blog",
   description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
   // base: '/', // 默认'/'。如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
