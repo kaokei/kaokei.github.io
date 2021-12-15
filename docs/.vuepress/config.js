d8f0774a3308b776cfc5964d04b93bc524730d77
kaokei <kaokei.rst@gmail.com>
2021-12-15 11:03:11 +0800

    增加谷歌统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,90 +1,91 @@
-const htmlModules = require('./config/htmlModules.js');
+const htmlModules = require("./config/htmlModules.js");
+
+//vuepress配置百度广告：https://www.sofineday.com/vuepress-baidu-advertisement.html#_4-%E9%85%8D%E7%BD%AE%E5%B9%BF%E5%91%8A
 
 module.exports = {
-  theme: 'vdoing', // 使用npm包主题
+  theme: "vdoing", // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "kaokei's blog",
-  description: 'web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。',
+  description:
+    "web前端技术博客,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,React,python,css3,html5,Node,git,github等技术文章。",
   // base: '/', // 默认'/'。如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
   // 主题配置
   themeConfig: {
     // 导航配置
     nav: [
-      { text: '首页', link: '/' },
+      { text: "首页", link: "/" },
       {
-        text: '前端',
-        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        text: "前端",
+        link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
           // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
           {
-            text: '前端文章',
-            items: [
-              { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-            ],
+            text: "前端文章",
+            items: [{ text: "JavaScript", link: "/pages/8143cc480faf9a11/" }],
           },
           {
-            text: '学习笔记',
+            text: "学习笔记",
             items: [
-              { text: '《JavaScript教程》', link: '/note/javascript/' },
-              { text: '《JavaScript高级程序设计》', link: '/note/js/' },
-              { text: '《ES6 教程》', link: '/note/es6/' },
-              { text: '《Vue》', link: '/note/vue/' },
-              { text: '《React》', link: '/note/react/' },
+              { text: "《JavaScript教程》", link: "/note/javascript/" },
+              { text: "《JavaScript高级程序设计》", link: "/note/js/" },
+              { text: "《ES6 教程》", link: "/note/es6/" },
+              { text: "《Vue》", link: "/note/vue/" },
+              { text: "《React》", link: "/note/react/" },
               {
-                text: '《TypeScript 从零实现 axios》',
-                link: '/note/typescript-axios/',
+                text: "《TypeScript 从零实现 axios》",
+                link: "/note/typescript-axios/",
               },
               {
-                text: '《Git》',
-                link: '/note/git/',
+                text: "《Git》",
+                link: "/note/git/",
               },
               {
-                text: 'TypeScript',
-                link: '/pages/51afd6/',
+                text: "TypeScript",
+                link: "/pages/51afd6/",
               },
               {
-                text: 'JS设计模式总结',
-                link: '/pages/4643cd/',
+                text: "JS设计模式总结",
+                link: "/pages/4643cd/",
               },
             ],
           },
         ],
       },
       {
-        text: '页面',
-        link: '/ui/',
+        text: "页面",
+        link: "/ui/",
         items: [
-          { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+          { text: "HTML", link: "/pages/8309a5b876fc95e3/" },
+          { text: "CSS", link: "/pages/0a83b083bdf257cb/" },
         ],
       },
       {
-        text: '技术',
-        link: '/technology/',
+        text: "技术",
+        link: "/technology/",
         items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+          { text: "技术文档", link: "/pages/9a7ee40fc232253e/" },
+          { text: "GitHub技巧", link: "/pages/4c778760be26d8b3/" },
+          { text: "Nodejs", link: "/pages/117708e0af7f0bd9/" },
+          { text: "博客搭建", link: "/pages/41f87d890d0a02af/" },
         ],
       },
       {
-        text: '更多',
-        link: '/more/',
+        text: "更多",
+        link: "/more/",
         items: [
-          { text: '学习', link: '/pages/f2a556/' },
-          { text: '面试', link: '/pages/aea6571b7a8bae86/' },
-          { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-          { text: '实用技巧', link: '/pages/baaa02/' },
-          { text: '友情链接', link: '/friends/' },
+          { text: "学习", link: "/pages/f2a556/" },
+          { text: "面试", link: "/pages/aea6571b7a8bae86/" },
+          { text: "心情杂货", link: "/pages/2d615df9a36a98ed/" },
+          { text: "实用技巧", link: "/pages/baaa02/" },
+          { text: "友情链接", link: "/friends/" },
         ],
       },
-      { text: '关于', link: '/about/' },
+      { text: "关于", link: "/about/" },
       {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
+        text: "收藏",
+        link: "/pages/beb6c0bd8a66cea6/",
         // items: [
         //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
         //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
@@ -92,23 +93,23 @@ module.exports = {
         // ],
       },
       {
-        text: '索引',
-        link: '/archives/',
+        text: "索引",
+        link: "/archives/",
         items: [
-          { text: '分类', link: '/categories/' },
-          { text: '标签', link: '/tags/' },
-          { text: '归档', link: '/archives/' },
+          { text: "分类", link: "/categories/" },
+          { text: "标签", link: "/tags/" },
+          { text: "归档", link: "/archives/" },
         ],
       },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.svg', // 导航栏logo
-    repo: 'kaokei/kaokei.github.io', // 导航栏右侧生成Github链接
+    logo: "/img/logo.svg", // 导航栏logo
+    repo: "kaokei/kaokei.github.io", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
-    docsDir: 'docs', // 编辑的文件夹
+    lastUpdated: "上次更新", // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+    docsDir: "docs", // 编辑的文件夹
     editLinks: true, // 启用编辑
-    editLinkText: '编辑',
+    editLinkText: "编辑",
 
     //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
 
@@ -140,19 +141,20 @@ module.exports = {
     // pageButton: false, // 是否显示快捷翻页按钮，默认true
 
     // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
-    sidebar: 'structuring',
+    sidebar: "structuring",
 
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
     author: {
-      name: 'kaokei', // 必需
-      link: 'https://github.com/kaokei', // 可选的
+      name: "kaokei", // 必需
+      link: "https://github.com/kaokei", // 可选的
     },
 
     // 博主信息，显示在首页侧边栏
     blogger: {
-      avatar: 'https://cdn.jsdelivr.net/gh/kaokei/static-cdn/logo/white/logo@512.png',
-      name: 'kaokei',
-      slogan: '好好学习，天天向上',
+      avatar:
+        "https://cdn.jsdelivr.net/gh/kaokei/static-cdn/logo/white/logo@512.png",
+      name: "kaokei",
+      slogan: "好好学习，天天向上",
     },
 
     // 社交图标，显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social
@@ -160,19 +162,19 @@ module.exports = {
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加
       icons: [
         {
-          iconClass: 'icon-youjian',
-          title: '发邮件',
-          link: 'mailto:kaokei.rst@gmail.com',
+          iconClass: "icon-youjian",
+          title: "发邮件",
+          link: "mailto:kaokei.rst@gmail.com",
         },
         {
-          iconClass: 'icon-github',
-          title: 'GitHub',
-          link: 'https://github.com/kaokei',
+          iconClass: "icon-github",
+          title: "GitHub",
+          link: "https://github.com/kaokei",
         },
         {
-          iconClass: 'icon-erji',
-          title: '听音乐',
-          link: 'https://music.163.com/#/playlist?id=755597173',
+          iconClass: "icon-erji",
+          title: "听音乐",
+          link: "https://music.163.com/#/playlist?id=755597173",
         },
       ],
     },
@@ -185,28 +187,49 @@ module.exports = {
     },
 
     // 插入hmtl(广告)模块
-    htmlModules
+    htmlModules,
   },
 
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ["link", { rel: "icon", href: "/img/favicon.ico" }], //favicons，资源放在public文件夹
     [
-      'meta',
+      "meta",
       {
-        name: 'keywords',
-        content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
+        name: "keywords",
+        content:
+          "前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
       },
     ],
-    ['meta', { name: 'baidu-site-verification', content: 'code-9N9fEhS90Y' }], // 百度统计的站长验证（你可以去掉）
-    ['meta', { name: 'baidu_union_verify', content: '1edbab67fb9cc5f891cf345fd76e23b8' }], // 百度联盟的站长验证（你可以去掉）
-    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ["meta", { name: "baidu-site-verification", content: "code-9N9fEhS90Y" }], // 百度统计的站长验证（你可以去掉）
     [
-      'script',
+      "meta",
       {
-        'data-ad-client': 'ca-pub-3912182864105528',
-        async: 'async',
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+        name: "baidu_union_verify",
+        content: "1edbab67fb9cc5f891cf345fd76e23b8",
+      },
+    ], // 百度联盟的站长验证（你可以去掉）
+    ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-EYD6NYPT3M",
+      },
+    ], // 谷歌分析的sdk
+    [
+      "script",
+      {},
+      [
+        "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-EYD6NYPT3M');",
+      ],
+    ], // 谷歌分析的初始化代码
+    [
+      "script",
+      {
+        "data-ad-client": "ca-pub-3912182864105528",
+        async: "async",
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
       },
     ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
   ],
@@ -219,51 +242,54 @@ module.exports = {
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
 
-    'vuepress-plugin-baidu-autopush', // 百度自动推送
+    "vuepress-plugin-baidu-autopush", // 百度自动推送
 
     // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
     [
-      'thirdparty-search',
+      "thirdparty-search",
       {
         thirdparty: [
           // 可选，默认 []
           {
-            title: '在MDN中搜索',
-            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+            title: "在MDN中搜索",
+            frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
+            behindUrl: "", // 搜索链接的后面部分，可选，默认 ''
           },
           {
-            title: '在Runoob中搜索',
-            frontUrl: 'https://www.runoob.com/?s=',
+            title: "在Runoob中搜索",
+            frontUrl: "https://www.runoob.com/?s=",
           },
           {
-            title: '在Vue API中搜索',
-            frontUrl: 'https://cn.vuejs.org/v2/api/#',
+            title: "在Vue API中搜索",
+            frontUrl: "https://cn.vuejs.org/v2/api/#",
           },
           {
-            title: '在Bing中搜索',
-            frontUrl: 'https://cn.bing.com/search?q=',
+            title: "在Bing中搜索",
+            frontUrl: "https://cn.bing.com/search?q=",
           },
           {
-            title: '通过百度搜索本站的',
-            frontUrl: 'https://www.baidu.com/s?wd=site%3Akaokei.com%20',
+            title: "通过百度搜索本站的",
+            frontUrl: "https://www.baidu.com/s?wd=site%3Akaokei.com%20",
           },
         ],
       },
     ],
 
     [
-      'one-click-copy', // 代码块复制按钮
+      "one-click-copy", // 代码块复制按钮
       {
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        copySelector: [
+          'div[class*="language-"] pre',
+          'div[class*="aside-code"] aside',
+        ], // String or Array
+        copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
         showInMobile: false, // whether to display on the mobile side, default: false.
       },
     ],
 
     [
-      'demo-block', // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
+      "demo-block", // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
       {
         settings: {
           // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
@@ -276,48 +302,47 @@ module.exports = {
       },
     ],
     [
-      'vuepress-plugin-zooming', // 放大图片
+      "vuepress-plugin-zooming", // 放大图片
       {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
         options: {
-          bgColor: 'rgba(0,0,0,0.6)',
+          bgColor: "rgba(0,0,0,0.6)",
         },
       },
     ],
     [
-      'vuepress-plugin-baidu-tongji', // 百度统计 （你可以去掉）
+      "vuepress-plugin-baidu-tongji", // 百度统计 （你可以去掉）
       {
-        hm: '35570fd61dde16af9949ec6af1c52ff8',
+        hm: "35570fd61dde16af9949ec6af1c52ff8",
       },
     ],
     [
-      'vuepress-plugin-comment', // 评论
+      "vuepress-plugin-comment", // 评论
       {
-        choosen: 'gitalk',
+        choosen: "gitalk",
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'kaokei', // GitHub仓库所有者
-          admin: ['kaokei'], // 对仓库有写权限的人
+          clientID: "a6e1355287947096b88b",
+          clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
+          repo: "blog-gitalk-comment", // GitHub 仓库
+          owner: "kaokei", // GitHub仓库所有者
+          admin: ["kaokei"], // 对仓库有写权限的人
           // distractionFreeMode: true,
-          pagerDirection: 'last', // 'first'正序 | 'last'倒序
-          id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-          title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-          body:
-            '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+          pagerDirection: "last", // 'first'正序 | 'last'倒序
+          id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
+          title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
+          labels: ["Gitalk", "Comment"], // GitHub issue 的标签
+          body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
         },
       },
     ],
     [
-      '@vuepress/last-updated', // "上次更新"时间格式
+      "@vuepress/last-updated", // "上次更新"时间格式
       {
         transformer: (timestamp, lang) => {
-          const dayjs = require('dayjs') // https://day.js.org/
-          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+          const dayjs = require("dayjs"); // https://day.js.org/
+          return dayjs(timestamp).format("YYYY/MM/DD, HH:mm:ss");
         },
       },
     ],
   ],
-}
+};
