4688087c41999a567818f854f4a7cc33cdba635d
kaokei <kaokei.rst@gmail.com>
2021-12-17 12:02:55 +0800

    初始化目录页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,82 +17,36 @@ module.exports = {
   themeConfig: {
     // 导航配置
     nav: [
-      { text: "首页", link: "/" },
       {
-        text: "前端",
-        link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        text: "随笔",
+        link: "/note/",
         items: [
-          // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-          {
-            text: "前端文章",
-            items: [{ text: "JavaScript", link: "/pages/8143cc480faf9a11/" }],
-          },
-          {
-            text: "学习笔记",
-            items: [
-              { text: "《JavaScript教程》", link: "/note/javascript/" },
-              { text: "《JavaScript高级程序设计》", link: "/note/js/" },
-              { text: "《ES6 教程》", link: "/note/es6/" },
-              { text: "《Vue》", link: "/note/vue/" },
-              { text: "《React》", link: "/note/react/" },
-              {
-                text: "《TypeScript 从零实现 axios》",
-                link: "/note/typescript-axios/",
-              },
-              {
-                text: "《Git》",
-                link: "/note/git/",
-              },
-              {
-                text: "TypeScript",
-                link: "/pages/51afd6/",
-              },
-              {
-                text: "JS设计模式总结",
-                link: "/pages/4643cd/",
-              },
-            ],
-          },
+          { text: "鸡汤", link: "/note/soul-chicken-soup/" },
+          { text: "吐槽", link: "/note/tucao/" },
+          { text: "笑话", link: "/note/joke/" },
+          { text: "头脑风暴", link: "/note/brainstorming/" },
         ],
       },
       {
-        text: "页面",
-        link: "/ui/",
+        text: "系列笔记",
+        link: "/series/",
         items: [
-          { text: "HTML", link: "/pages/8309a5b876fc95e3/" },
-          { text: "CSS", link: "/pages/0a83b083bdf257cb/" },
+          { text: "Git", link: "/series/git/" },
+          { text: "Shell", link: "/series/shell/" },
+          { text: "编译器", link: "/series/compiler/" },
+          { text: "操作系统", link: "/series/os/" },
+          { text: "计算机网络", link: "/series/network/" },
         ],
       },
       {
-        text: "技术",
-        link: "/technology/",
+        text: "项目文档",
+        link: "/project/",
         items: [
-          { text: "技术文档", link: "/pages/9a7ee40fc232253e/" },
-          { text: "GitHub技巧", link: "/pages/4c778760be26d8b3/" },
-          { text: "Nodejs", link: "/pages/117708e0af7f0bd9/" },
-          { text: "博客搭建", link: "/pages/41f87d890d0a02af/" },
+          { text: "di", link: "/project/di/" },
+          { text: "use-vue-service", link: "/project/use-vue-service/" },
+          { text: "use-react-service", link: "/project/use-react-service/" },
+          { text: "use-solid-service", link: "/project/use-solid-service/" },
         ],
-      },
-      {
-        text: "更多",
-        link: "/more/",
-        items: [
-          { text: "学习", link: "/pages/f2a556/" },
-          { text: "面试", link: "/pages/aea6571b7a8bae86/" },
-          { text: "心情杂货", link: "/pages/2d615df9a36a98ed/" },
-          { text: "实用技巧", link: "/pages/baaa02/" },
-          { text: "友情链接", link: "/friends/" },
-        ],
-      },
-      { text: "关于", link: "/about/" },
-      {
-        text: "收藏",
-        link: "/pages/beb6c0bd8a66cea6/",
-        // items: [
-        //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-        //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-        //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-        // ],
       },
       {
         text: "索引",
