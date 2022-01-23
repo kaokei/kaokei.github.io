05de2d2476f99274109ae89f4da1e85b04da1be6
kaokei <kaokei.rst@gmail.com>
2022-01-23 23:08:05 +0800

    调整导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,13 +32,16 @@ module.exports = {
         ],
       },
       {
-        text: "索引",
+        text: "分类",
+        link: "/categories/",
+      },
+      {
+        text: "标签",
+        link: "/tags/",
+      },
+      {
+        text: "归档",
         link: "/archives/",
-        items: [
-          { text: "分类", link: "/categories/" },
-          { text: "标签", link: "/tags/" },
-          { text: "归档", link: "/archives/" },
-        ],
       },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
