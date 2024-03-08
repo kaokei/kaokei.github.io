b96c2da5adafe085daeb75f3b2319ce50571613c
kaokei <kaokei.rst@gmail.com>
2024-03-08 14:22:28 +0800

    init project v2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-lodash",
+    "@vueuse/nuxt",
+    "@zadigetvoltaire/nuxt-gtm",
+  ],
+  vite: {
+    esbuild: {
+      tsconfigRaw: {},
+    },
+  },
+  gtm: {
+    id: "GTM-123456",
+  },
+});
---
+++ pages/game/console/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+const age = ref(0);
+</script>
+
+<template>
+  <h1>{{ age }}</h1>
+</template>
