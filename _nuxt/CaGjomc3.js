3c31f85fde02df2ae4c7f11d26e188e50871a079
kaokei <kaokei.rst@gmail.com>
2024-12-24 15:55:16 +0800

    Deploying to gh-pages from @ kaokei/kaokei.github.io@4ccc84c2445ec26e48daa351304d4056a417b8f6 🚀

---
+++ _nuxt/CaGjomc3.js
@@ -0,0 +1,1 @@
+import{u as s}from"./uPm2a2Or.js";import{e as t,o as l,c as a,a as e}from"./BxyW0aqM.js";import"./BDy7lJrC.js";const n={class:"px-6 py-10"},u=t({__name:"index",setup(r){return s({title:"kaokei"}),(p,o)=>(l(),a("div",n,o[0]||(o[0]=[e("article",{class:"prose mb-4 lg:prose-xl"},[e("h2",null,"URL格式化"),e("p",null,"好好学习，天天向上")],-1),e("div",null,null,-1)])))}});export{u as default};
