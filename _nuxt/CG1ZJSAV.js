4de626e5fed1a2d93d57df7d12a4db6f7fe6eb81
kaokei <kaokei.rst@gmail.com>
2024-12-24 10:55:24 +0800

    Deploying to gh-pages from @ kaokei/kaokei.github.io@7292810bc18474f228d1bb36452ed5a7d3eafcab 🚀

---
+++ _nuxt/CG1ZJSAV.js
@@ -0,0 +1,1 @@
+import{E as s}from"./Y2wVbCCS.js";import{u}from"./BZAjFUj_.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
