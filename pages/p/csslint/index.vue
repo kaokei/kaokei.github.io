4ccc84c2445ec26e48daa351304d4056a417b8f6
kaokei <kaokei.rst@gmail.com>
2024-12-24 15:54:42 +0800

    增加工具页面 v2

---
+++ pages/p/csslint/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { definePageMeta, ref, useSeoMeta } from '#imports';
+
+definePageMeta({
+  name: 'p_csslint',
+  key: (route) => route.name as string,
+});
+
+useSeoMeta({
+  title: 'kaokei',
+});
+</script>
+
+<template>
+  <div class="px-6 py-10">
+    <article class="prose mb-4 lg:prose-xl">
+      <h2>CSS格式化</h2>
+      <p>好好学习，天天向上</p>
+    </article>
+
+    <div></div>
+  </div>
+</template>
